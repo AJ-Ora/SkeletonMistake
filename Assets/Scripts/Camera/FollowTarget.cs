5fd7252b52b38962f535b7d3908c9e34ec928fa2
AJ-Ora <antti-jussioravainen@kamk.fi>
2019-10-04 20:38:42 +0300

    Added a neat camera, and made multiple jumps possible.

---
+++ Assets/Scripts/Camera/FollowTarget.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SkeletonMistake
+{
+    [RequireComponent(typeof(Camera))]
+    public class FollowTarget : MonoBehaviour
+    {
+        [SerializeField] private Transform followTarget = null;
+        [SerializeField][Range(0.0f, 1.0f)] private float followSpeed = 0.7f;
+        [SerializeField] private Vector3 offset = -Vector3.forward;
+        [SerializeField] private bool lockX = true;
+        [SerializeField] private bool lockY = false;
+
+        private Camera cam = null;
+
+        private void Awake()
+        {
+            if (cam == null)
+            {
+                cam = GetComponent<Camera>();
+            }
+        }
+
+        void LateUpdate()
+        {
+            if (followTarget != null)
+            {
+                Vector3 newPos = Vector3.zero;
+
+                if (!lockX)
+                {
+                    newPos.x = Mathf.Lerp(transform.position.x, followTarget.position.x, followSpeed);
+                }
+
+                if (!lockY)
+                {
+                    newPos.y = Mathf.Lerp(transform.position.y, followTarget.position.y, followSpeed);
+                }
+
+                transform.position = newPos + offset;
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 namespace SkeletonMistake
 {
     [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(BoxCollider2D))]
     public class PlayerController : MonoBehaviour
     {
         [Header("Input")]
@@ -15,10 +16,13 @@ namespace SkeletonMistake
         [SerializeField] private float acceleration = 80.0f;
         [SerializeField] private float maxHorizontalVelocity = 8.0f;
         [SerializeField] private float jumpForce = 5.0f;
+        [SerializeField] private int maxJumps = 3;
 
         private Rigidbody2D rigid = null;
+        private BoxCollider2D col = null;
         private bool isJumping = false;
         private bool isGrounded = false;
+        private int currentJumps = 0;
 
         private void Awake()
         {
@@ -26,17 +30,54 @@ namespace SkeletonMistake
             {
                 rigid = GetComponent<Rigidbody2D>();
             }
+
+            if (col == null)
+            {
+                col = GetComponent<BoxCollider2D>();
+            }
         }
-        
+
         private void FixedUpdate()
         {
+            /* ----- CHECK IF GROUNDED ----- */
+
+            RaycastHit2D hitLeft = Physics2D.Raycast(transform.position + Vector3.left * col.size.x / 2 + Vector3.down * col.size.y / 2, Vector2.down, 0.05f);
+            RaycastHit2D hitRight = Physics2D.Raycast(transform.position + Vector3.right * col.size.x / 2 + Vector3.down * col.size.y / 2, Vector2.down, 0.05f);
+            
+            if (hitLeft.collider != null || hitRight.collider != null)
+            {
+                if (hitLeft.collider != col && hitRight.collider != col)
+                {
+                    isGrounded = true;
+                    currentJumps = maxJumps;
+                }
+                else
+                {
+                    isGrounded = false;
+                }
+            }
+            else
+            {
+                isGrounded = false;
+            }
+
+            Debug.DrawRay(transform.position + Vector3.left * col.size.x / 2 + Vector3.down * col.size.y / 2, Vector2.down * 0.05f, isGrounded ? Color.green : Color.yellow);
+            Debug.DrawRay(transform.position + Vector3.right * col.size.x / 2 + Vector3.down * col.size.y / 2, Vector2.down * 0.05f, isGrounded ? Color.green : Color.yellow);
+
+            /* ----- DO SOME PHYSICS BASED ON INPUTS ----- */
+
             if (Input.GetButton(inputJump))
             {
                 if (!isJumping)
                 {
                     isJumping = true;
-                    rigid.velocity = new Vector2(rigid.velocity.x, 0.0f);
-                    rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+                    if (currentJumps > 0)
+                    {
+                        rigid.velocity = new Vector2(rigid.velocity.x, 0.0f);
+                        rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                        currentJumps--;
+                    }
                 }
             }
             else
