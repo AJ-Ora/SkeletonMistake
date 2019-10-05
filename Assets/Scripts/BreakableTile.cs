36e940d54799230cd8754cf84b870ea1c7b0233e
AJ-Ora <antti-jussioravainen@kamk.fi>
2019-10-05 12:21:08 +0300

    Added particle effects to breakable tiles.

---
+++ Assets/Scripts/BreakableTile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SkeletonMistake
+{
+    public class BreakableTile : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem effectWhenDestroyed = null;
+
+        private void OnDestroy()
+        {
+            if (effectWhenDestroyed != null)
+            {
+                Instantiate(effectWhenDestroyed, transform.position, effectWhenDestroyed.transform.rotation, null);
+            }
+        }
+    }
+}
